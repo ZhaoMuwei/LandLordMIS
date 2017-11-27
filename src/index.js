790c9852bea3d0990c307fa25487e16d43663063
ZhaoMuwei <zhaomuwei@baidu.com>
2017-11-27 20:58:52 +0800

    Login Temp

--- src/index.js
+++ src/index.js
@@ -2,10 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'normalize.css';
 import registerServiceWorker from './registerServiceWorker';
+import Login from './components/Login/Login';
 import './index.css';
 
 ReactDOM.render(
-    <p>hello</p>,
+    <div className="login-wrapper">
+        <div className="login-content">
+            <h2>Hey, man</h2>
+            <Login />
+        </div>
+    </div>,
     document.getElementById('root')
 );
 
