d53a2a880a173cab573e78098ad1c9857a735e5a
zhaomuwei <zhaomuwei@baidu.com>
2017-11-27 19:47:59 +0800

    A very basic Master.

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'normalize.css';
+import registerServiceWorker from './registerServiceWorker';
+import './index.css';
+
+ReactDOM.render(
+    <p>hello</p>,
+    document.getElementById('root')
+);
+
+registerServiceWorker();
